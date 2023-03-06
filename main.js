278520ec0882bfaaff240e4145dec3be077f37fe
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2023-03-06 23:42:37 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+  
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+    inputNumber.oninput = () => {
+        if(inputNumber.valu.length > inputNumber.maxlength) inputNumber.valu
+        = inputNumber.valu.slice(0, inputNumber.maxlength);
+    };
+
+    
+});
+
+
+
+
+
+ 
+ 
